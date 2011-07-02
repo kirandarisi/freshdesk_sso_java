45fb65d58c0c57314201f4da9f8496a50e80f65f
Kiran Darisi <kiran.darisi@gmail.com>
2011-07-02 23:53:02 +0530

    first commit

---
+++ FreshDeskSSOServlet.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FreshDeskSSOServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7027717204177362374L;
+	private final String BASE_URL = "http://demo.freshdesk.com/";
+	private final String sharedSecret = "df02b88d971c78807a644adeae260919";
+
+
+	@Override
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		try {
+
+				String url = this.getSSOURL();
+				response.sendRedirect(url);
+
+			} catch (Exception e) {
+				//Handle appropriate code
+			}
+	}
+	
+	private static String getHash(String text) throws Exception {
+		    MessageDigest m=MessageDigest.getInstance("MD5");
+ 	     	m.update(text.getBytes(),0,text.length());
+   			return ""+new BigInteger(1,m.digest()).toString(16);
+	}
+
+
+
+	private String getSSOURL() {
+
+		String hash;
+		String url = null;
+		User user; //Get the user details using your current authentication system
+		String name = "kiran darisi";// Full name of the user
+		String email = "kiran@freshdesk.com";// Email of the user	
+		hash = getHash(name + email + authSecret);
+		
+		try {		
+			
+			url = BASE_URL + "?name="+name+"&email="+email+"&hash=" + hash; 
+			
+		}catch (Exception e) {
+			//Handle appropriate code
+			System.out.println("There is an exception while constructing the URL");
+			e.printStackTrace();
+		}	
+		
+		return url;
+			
+	}
+}
