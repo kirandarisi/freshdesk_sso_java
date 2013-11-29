a93b61e6cf1934ee272977f08b6c73be041f48d3
Kiran Darisi <kiran.darisi@gmail.com>
2013-11-29 17:19:32 +0530

    Added support for HMAC

---
+++ FDHmac.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.io.File;
+import java.io.DataInputStream;
+import java.io.FileInputStream ;
+import java.lang.reflect.UndeclaredThrowableException;
+
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidKeyException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class FDHmac
+{
+
+
+    private static final long serialVersionUID = 7027717204177362374L;
+    private static final String BASE_URL = "https://demo.freshdesk.com/login/sso";
+    private static final String sharedSecret = "75b5deb96tthbf2bb9e72f55ae22a0ffb";
+
+    public static String hashToHexString(byte[] byteData)
+    {
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+            String hex = Integer.toHexString(0xff & byteData[i]);
+            // NB! E.g.: Integer.toHexString(0x0C) will return "C", not "0C"            
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    private static String getHMACHash(String name,String email,long timeInMillis) throws Exception {
+        byte[] keyBytes = sharedSecret.getBytes();
+        String movingFact =name+email+timeInMillis;
+        byte[] text = movingFact.getBytes();
+        
+        String hexString = "";
+        Mac hmacMD5;
+        try {
+          hmacMD5 = Mac.getInstance("HmacMD5");
+          SecretKeySpec macKey = new SecretKeySpec(keyBytes, "RAW");
+          hmacMD5.init(macKey);
+          byte[] hash =  hmacMD5.doFinal(text);
+          hexString = hashToHexString(hash);
+
+        } catch (Exception nsae) {
+            System.out.println("Caught the exception");
+        }
+                return hexString;
+    }
+
+    public static void main(String[] args)
+      throws NoSuchAlgorithmException, InvalidKeyException
+
+    {
+
+        String hash;
+        String url = null;
+        //User user; //Get the user details using your current authentication system
+        String name = "Freshdesk";// Full name of the user
+        String email = "kiran@freshdesk.com";// Email of the user
+        long timeInMillis = System.currentTimeMillis(); 
+        
+        
+        try {       
+            
+            hash = getHMACHash(name,email,timeInMillis);
+            url = BASE_URL + "?name="+name+"&email="+email+"&timestamp="+timeInMillis+"&hash=" + hash; 
+            
+        }catch (Exception e) {
+            //Handle appropriate code
+            System.out.println("There is an exception while constructing the URL");
+            e.printStackTrace();
+        }   
+        System.out.println(url);
+    }
+}
